dcac0a83be84398ab3c2c24aa9e5bdbe7a216e73
Jin <jinpark1504@gmail.com>
2020-05-21 18:49:10 -0400

    Download links
    Modify index.js and utilities.js for recursive iteration

--- utilities.js
+++ utilities.js
@@ -3,14 +3,32 @@
 const urlParse = require('url').parse
 const slug = require('slug')
 const path = require('path')
+const urlResolve = require('url').resolve;
+const cheerio = require('cheerio');
 
 module.exports.urlToFilename = url => {
-    const parsedUrl = urlParse(url)
-    const urlPath = parsedUrl.path.split('/')
-                    .filter(component => component !== '')
-                    .map(component => slug(component, {remove: null}))
-                    .join('/')
-    let filename = path.join(parsedUrl.hostname, urlPath)
-    if (!path.extname(filename).match(/htm/)) filename += '.html'
-    return filename
+  const parsedUrl = urlParse(url)
+  const urlPath = parsedUrl.path.split('/')
+                  .filter(component => component !== '')
+                  .map(component => slug(component, {remove: null}))
+                  .join('/')
+  let filename = path.join(parsedUrl.hostname, urlPath)
+  if (!path.extname(filename).match(/htm/)) filename += '.html'
+  return filename
 }
+
+module.exports.getLinkUrl = (currentUrl, element) => {
+  const link = urlResolve(currentUrl, element.attribs.href || "")
+  const parsedLink = urlParse(link)
+  const currentParsedUrl = urlParse(currentUrl)
+  if (parsedLink.hostname !== currentParsedUrl.hostname || !parsedLink.pathname) return null
+  return link
+};
+
+// get list of all links
+module.exports.getPageLinks = (currentUrl, body) => {
+  console.log('here', !true)
+  return [].slice.call(cheerio.load(body)('a'))
+        .map(element => module.exports.getLinkUrl(currentUrl, element))
+        .filter(element => !!element)
+};
